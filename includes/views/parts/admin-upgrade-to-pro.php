79038702847a27953a96c9f7cf937287e0f0b749
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-20 20:35:17 +0000

    Version 1.3.1 - combined CSS, improved admin texts, fixed remote call bug for older PHP versions.
    git-svn-id: https://plugins.svn.wordpress.org/mailchimp-for-wp/tags/1.3.1@790946 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/views/parts/admin-upgrade-to-pro.php
@@ -0,0 +1,9 @@
+<div class="mc4wp-box" id="mc4wp-upgrade-box">
+	<h3>MailChimp for WordPress Pro</h3>
+	
+	<p><em>This plugin comes with a premium version, you will love it.</em></p>
+
+	<p>Better forms, easier styling and statistics. Just a few of the many benefits of upgrading.</p>
+
+	<p><a href="http://dannyvankooten.com/wordpress-plugins/mailchimp-for-wordpress/?utm_source=lite-plugin&utm_medium=link&utm_campaign=upgrade-box">More information about MailChimp for WP Pro &raquo;</a></p>
+</div>
