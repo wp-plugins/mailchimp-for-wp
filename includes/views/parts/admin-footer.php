6f7fae5353c829f46322c0c63f984dfaa63c18a7
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-26 22:28:55 +0000

    Version 1.4.4 - various admin usability improvements + CSS and placeholder improvements
    git-svn-id: https://plugins.svn.wordpress.org/mailchimp-for-wp/tags/1.4.4@811081 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/views/parts/admin-footer.php
@@ -0,0 +1,4 @@
+<br style="clear:both;" />
+<p class="copyright-notice">Enjoying this plugin? <a href="http://dannyvankooten.com/mailchimp-for-wordpress/?utm_source=lite-plugin&utm_medium=link&utm_campaign=footer-link">Upgrade to MailChimp for WordPress Pro now</a> for an even better plugin, you will love it.</p>
+<p class="help">What's next for MailChimp for WordPress? Submit your feature requests or vote for new features using <a href="http://www.google.com/moderator/#15/e=20c6b7&t=20c6b7.40">this Google Moderator tool.</a></p>
+<p class="copyright-notice">This plugin is not developed by or affiliated with MailChimp in any way.</p>
