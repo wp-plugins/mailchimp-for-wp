7f3ad9b467ad85472e337eabd1a92bf434096fb4
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-09 20:51:51 +0000

    cleaned up
    git-svn-id: https://plugins.svn.wordpress.org/mailchimp-for-wp/trunk@817716 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/views/parts/admin-nav.php
+++
@@ -1,5 +0,0 @@
-<h2 class="nav-tab-wrapper">  
-	<a class="nav-tab <?php if($tab == 'api-settings') echo 'nav-tab-active'; ?>" href="admin.php?page=mc4wp-lite">API settings</a>
-	<a class="nav-tab <?php if($tab == 'checkbox-settings') echo 'nav-tab-active'; ?>" href="admin.php?page=mc4wp-lite-checkbox-settings">Checkbox settings</a>
-	<a class="nav-tab <?php if($tab == 'form-settings') echo 'nav-tab-active'; ?>" href="admin.php?page=mc4wp-lite-form-settings">Form settings</a>
-</h2>
